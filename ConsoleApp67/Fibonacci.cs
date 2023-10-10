9cdef52dc22c1a0e33d302e5354c9050ea087505
jkrzyzan1 <jkrzyzan@wp.pl>
2023-10-10 14:39:41 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp67/Fibonacci.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp67
+{
+    internal class Fibonacci
+    {
+        //wrzucić parametry min,max
+        public static void Sequence(int max)
+        {
+            var timer = new Stopwatch();
+            int counter = 0;
+            int x = 0;
+            int y = 1;
+            int z;
+            for (int i = 2; i <= max; i++)
+            {
+                if (i == 30)
+                    timer.Start();
+                if (i>= 30 && i<47)
+                {
+                    z = x + y;
+                    counter++;
+                    x = y;
+                    counter++;
+                    y = z;
+                    counter++;
+                }
+
+                else if (i == 47)
+                {
+                    z = x + y;
+                    counter++;
+                    timer.Stop();
+                }
+                else
+                {
+                    z = x + y;
+                    x = y;
+                    y = z;
+                }
+            }
+            Console.WriteLine($"Number of operations: {counter}");
+            Console.WriteLine($"Time: {timer.Elapsed}");
+        }
+    }
+}
---
+++ ConsoleApp67/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci.Sequence(47);
+            Console.Read();
+        }
+    }
+}
